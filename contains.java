441219389630a0a7851ce79727fe2070e106bfb9
SIVANANTHAM2005 <sivanandham1313@gmail.com>
2025-03-21 09:11:34 +0530

    Add files via upload

---
+++ contains.java
@@ -0,0 +1,68 @@
+package day2string;
+import java.util.*;
+public class contains {
+	public  static void main(String[ ] args) {
+		/*String str ="java programming";
+		System.out.println(str.contains("java"));
+		System.out.println(str.contains("phython"));
+	}*/
+		
+	/*String s1 =new String("JavaDeveloper");
+	String s2="JavaDeveloper";
+	String s3= s1.intern();
+	System.out.println(s1==s2);
+	System.out.println(s2==s3);
+   }*/
+		
+	/*StringBuilder sb= new StringBuilder("Hello");
+	// Append
+	sb.append(" world");
+	System.out.println("After append:" + sb);
+		
+		
+		
+		
+		
+    }*/
+		
+		/*StringBuilder sb= new StringBuilder("Hello");
+		// Append
+		sb.append(" world");
+		System.out.println("After append:" + sb);
+		sb.insert(5,",");
+		System.out.println("After insert:  " + sb);
+			sb.replace(6,11, "Java");
+	System.out.println("After replace: " + sb);
+sb.reverse();
+	System.out.println("After reverse:" + sb);
+		
+	}*/
+		
+		StringBuilder sb= new StringBuilder("Hello");
+		// Append
+		sb.append(" world");
+		System.out.println("After append:" + sb);
+		sb.insert(5,",");
+		System.out.println("After insert:  " + sb);
+			sb.replace(6,11, "Java");
+	System.out.println("After replace: " + sb);
+sb.reverse();
+	System.out.println("After reverse:" + sb);
+	
+	StringBuffer sb1 = new StringBuffer("Java");
+	sb1.append("Programming");
+	System.out.println("StringBuffer:" + sb1);
+	}	
+		
+		
+		
+		
+  }		
+		
+		
+		
+		
+	
+	
+		
+		
